aa3a5fed5a43db5781c0b7667ad3b9ba7659cfd3
neelganta <43225244+neelganta@users.noreply.github.com>
2020-03-23 12:42:05 -0500

    Update NBA_app.py

--- NBA_app.py
+++ NBA_app.py
@@ -26,14 +26,14 @@ st.title('WNBA Lineup Machine')
 # st.title('NBA Lineup Machine')
 st.markdown('_Please see left sidebar for more details._')
 
-currentStats = pd.read_csv('https://github.com/neelganta/neel_project/edit/master/wnba19.csv') #Dynasty
+currentStats = pd.read_csv('https://raw.githubusercontent.com/neelganta/neel_project/master/2019wnba.csv')#, delimiter= None, error_bad_lines=False) #Dynasty
 # currentStats = pd.read_csv('https://raw.githubusercontent.com/neelganta/neel_project/master/2020stats_salary.csv') #Current
 regModel = pd.read_csv('https://raw.githubusercontent.com/neelganta/neel_project/master/WNBAreg.csv')
 regModel = regModel.fillna(0)
 # regModel = regModel.drop(columns=['Unnamed: 0'])
 
 y = regModel['NET_RATING'] 
-X = regModel.drop(['NET_RATING', 'MIN', 'Unnamed: 0'], axis =1)
+X = regModel.drop(['NET_RATING', 'MIN'], axis =1)
 # Fit the model below
 model1 =  lm.LinearRegression() #higher alpha (penality parameter), fewer predictors
 model1.fit(X, y)
@@ -52,7 +52,7 @@ players = list(players)
 
 
 player1 = st.selectbox('Select first player: (Example: Type "LAS" to find all Sparks)', players)
-player2 = st.selectbox('Select second player: (Example: Type "PG" to find all Point Guards)', players)
+player2 = st.selectbox('Select second player: (Example: Type "G" to find all Guards)', players)
 player3 = st.selectbox('Select third player: (Example: Type "Alexis" to find all players with the name Alexis)', players)
 player4 = st.selectbox('Select fourth player: ', players)
 player5 = st.selectbox('Select fifth player: ', players)
@@ -71,32 +71,38 @@ if(player1 != '2019 WNBA Players' and player2 != '2019 WNBA Players' and player3
 # if(len(playerlist) > 4 and len(playerlist) < 6):
     userdf = pd.DataFrame(playerlist)
     userdf['Player'] = userdf[0]
-
+    userdf = userdf.drop([0], axis = 1)
     merged = userdf.merge(right = currentStats, on ='Player')
-    merged['index'] = merged[0]
-    merged['index'] = 'lineup'
+    merged['index'] = 0
+    # merged['index'] = 'lineup'
     merged.set_index('index')
-    merged.drop(['Player'], axis =1)
+    # merged = merged.drop(['Player'], axis =1)
     dictionary = merged.groupby('index').apply(lambda dfg: dfg.drop('index', axis=1).to_dict(orient='list')).to_dict()
     converted = pd.DataFrame.from_dict(dictionary, orient= 'index')
     WNBA_converted = pd.concat([pd.DataFrame(col.tolist()).add_prefix(i) 
                         for i,col in converted.items()],axis = 1)
-    WNBA_converted.index = converted.index
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='5')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='6')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='7')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='8')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='9')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='10')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='11')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='12')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='13')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='14')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='20')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='21')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='22')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='23')))]
-    WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='24')))]
+    # WNBA_converted.index = converted.index
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='00')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='01')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='02')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='03')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='04')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='5')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='6')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='7')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='8')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='9')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='10')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='11')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='12')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='13')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='14')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='20')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='21')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='22')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='23')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='24')))]
+    # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='Player')))]
 
     st.write('Lineup DataFrame:')
     st.write(WNBA_converted)
@@ -131,34 +137,40 @@ if(player1 != '2019 WNBA Players' and player2 != '2019 WNBA Players' and player3
         t=list(itertools.permutations(playerlist,len(playerlist)))
         for i in range(0,len(t)):
             x = t[i]
-            userdf = pd.DataFrame(x)
+            userdf = pd.DataFrame(playerlist)
             userdf['Player'] = userdf[0]
-
+            userdf = userdf.drop([0], axis = 1)
             merged = userdf.merge(right = currentStats, on ='Player')
-            merged['index'] = merged[0]
-            merged['index'] = 'lineup'
+            merged['index'] = 0
+            # merged['index'] = 'lineup'
             merged.set_index('index')
-            merged.drop(['Player'], axis =1)
+            # merged = merged.drop(['Player'], axis =1)
             dictionary = merged.groupby('index').apply(lambda dfg: dfg.drop('index', axis=1).to_dict(orient='list')).to_dict()
             converted = pd.DataFrame.from_dict(dictionary, orient= 'index')
             WNBA_converted = pd.concat([pd.DataFrame(col.tolist()).add_prefix(i) 
                                 for i,col in converted.items()],axis = 1)
-            WNBA_converted.index = converted.index
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='5')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='6')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='7')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='8')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='9')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='10')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='11')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='12')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='13')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='14')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='20')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='21')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='22')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='23')))]
-            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='24')))]
+            # WNBA_converted.index = converted.index
+            WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='Player')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='00')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='01')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='02')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='03')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='04')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='5')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='6')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='7')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='8')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='9')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='10')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='11')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='12')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='13')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='14')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='20')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='21')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='22')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='23')))]
+            # WNBA_converted = WNBA_converted[WNBA_converted.columns.drop(list(WNBA_converted.filter(regex='24')))]
 #             new_df = new_df[new_df.columns.drop(list(new_df.filter(regex='Salary')))]
             user_pred = model1.predict(WNBA_converted)
             num = int(user_pred)
@@ -195,10 +207,9 @@ st.video(data = 'https://www.youtube.com/watch?v=le3cMnQ7_74&feature=youtu.be')
 st.markdown('_Presented by Neel Ganta._')
 
 st.sidebar.video(data = 'https://www.youtube.com/watch?v=le3cMnQ7_74&feature=youtu.be')
-st.sidebar.markdown('**ABOUT THE WNBA LINEUP MACHINE:**  After creating the _[NBA Lineup Machine](https://nba-lineup-machine.herokuapp.com)_, which allows the user to predict the Net Rating of any lineup possible in the current NBA, Neel Ganta went about to answer a similar set of questions in regards to the WNBA. The NBA Lineup Machine was first incepted roughly one year ago while Neel Ganta was pondering the current lineup problem in the NBA. Should teams go small? Three shooters? Five? How can we see what our team would look like with a player before trading for them? Seeing a problem and no publicly available solution, Neel decided to create what could be the next big GM tool. Please enjoy the WNBA Lineup Machine which allows you to input any five players in the WNBA, and utilizes a machine learning algorithm to predict an overall Net Rating for the lineup.')
+st.sidebar.markdown('**ABOUT THE WNBA LINEUP MACHINE:**  After creating the _[NBA Lineup Machine](https://nba-lineup-machine.herokuapp.com)_, which allows the user to predict the Net Rating of any lineup possible in the current NBA, Neel Ganta went about to answer a similar set of questions in regards to the WNBA. Should teams go small? Three shooters? Five? How can we see what our team would look like with a player before trading for them? Seeing a problem and no publicly available solution, Neel decided to create what could be the next big GM tool. Please enjoy the WNBA Lineup Machine which allows you to input any five players in the WNBA, and utilizes a machine learning algorithm to predict an overall Net Rating for the lineup.')
 st.sidebar.markdown('_**Poor: ** Net Rating **< 0** | **Average:** Net Rating **> 0** | **Good:** Net Rating **> 5** | **Excellent:** Net Rating **> 10** | **Hall of Fame:** Net Rating **> 20**_')
-st.sidebar.markdown('_Players, teams, and positions are searchable in the drop down selectors. For example: type "2012 to find all players in the year 2012. Type "CLE" to find all Cavaliers. Type "PG" to find all point guards. Type "James" to find all players with James in their name. Type "MVP" to find all MVPs from each year._')
-st.sidebar.markdown('_An asterisk "*" by a player name deontes a hall of fame inductee._')
+st.sidebar.markdown('_Players, teams, and positions are searchable in the drop down selectors. For example: Type "LAS" to find all Sparks. Type "F" to find all forwards. Type "James" to find all players with James in their name._')
 
 st.sidebar.markdown('**ABOUT NEEL GANTA**: Neel Ganta is graduating with a Finance and Computer Science degree from Kansas State, and completed internships at the Federal Reserve, JPMorgan Chase, the **Boston Celtics**, and currently serves as an analytics consultant for **Brad Underwood, Head Basketball Coach at the University of Illinois.** Neel grew up using his passion for basketball to connect with others, and can be found playing 5 on 5 in his local city league tournament or rec center. When he is taking a break from practicing dunks and _NBA_ three pointers, he is sharpening his machine learning skills and seeking new avenues to provide basketball insights.')
 # st.sidebar.video('https://youtu.be/-OoM5XvLo20')
